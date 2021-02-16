66384790ec58643d81b5845e4c6b512677122b5c
marafli <rafliandrianfauzi@gmail.com>
2021-02-16 16:32:29 +0700

    add command system

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -1,9 +1,22 @@
 from discord.ext.commands import Cog
+from random import choice, randint
+from discord.ext.commands import command
 
 
 class Fun(Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @command(name="test", aliases=["tst"])
+    async def test_command(self, ctx):
+        await ctx.send(f"{choice(('togi', 'agus', 'warman'))} {ctx.author.mention}")
+
+    @command(name="dice", aliases=["roll"])
+    async def roll_dice(self, ctx, die_string: str):
+        dice, value = (int(term) for term in die_string.split("d"))
+        rolls = [randint(i, value) for i in range(dice)]
+
+        await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum(rolls)}")
 
     @Cog.listener()
     async def on_ready(self):
