e647cad82111fd443f95ba3f8e5b7938b92332d3
marafli <rafliandrianfauzi@gmail.com>
2021-02-17 23:45:07 +0700

    add say command

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -3,16 +3,18 @@ from discord import Member
 from typing import Optional
 from random import choice, randint
 from discord.ext.commands import command
-
+from discord.errors import HTTPException
 
 class Fun(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # TEST COMMAND
     @command(name="test", aliases=["tst"])
     async def test_command(self, ctx):
         await ctx.send(f"{choice(('togi', 'agus', 'warman'))} {ctx.author.mention}")
 
+    # DICE COMMAND
     @command(name="dice", aliases=["roll"])
     async def roll_dice(self, ctx, die_string: str):
         dice, value = (int(term) for term in die_string.split("d"))
@@ -20,9 +22,15 @@ class Fun(Cog):
 
         await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum(rolls)}")
 
+
     @command(name="slap", aliases=["tenggeul"])
     async def slap_member(self, ctx, member: Member, *, reason: Optional[str] = "no reason"):
         await ctx.send(f"{ctx.author.mention} nenggeul {member.mention} pedah {reason}!")
+
+    @command(name="say", aliases=["ngomong"])
+    async def say_message(self, ctx, *, message):
+        await ctx.message.delete()
+        await ctx.send(message)
 
     @Cog.listener()
     async def on_ready(self):
