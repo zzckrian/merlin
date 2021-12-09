997e8c4538143a1e4e08de4bfebfdd4e3e4ac6f2
eskrimlezat <howtoind17@gmail.com>
2021-12-09 23:33:05 +0700

    add "about" command

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -1,9 +1,10 @@
-from discord.ext.commands import Cog
-from discord import Member
-from typing import Optional
 from random import choice, randint
+from typing import Optional
+
+from discord import Member
+from discord.ext.commands import Cog
 from discord.ext.commands import command
-from discord.errors import HTTPException
+
 
 class Fun(Cog):
     def __init__(self, bot):
@@ -12,7 +13,11 @@ class Fun(Cog):
     # TEST COMMAND
     @command(name="test", aliases=["tst"])
     async def test_command(self, ctx):
-        await ctx.send(f"{choice(('togi', 'agus', 'warman'))} {ctx.author.mention}")
+        await ctx.send(f"{choice(('bot is running', 'kefo'))} {ctx.author.mention}")
+
+    @command(name="about", aliases=["abt"])
+    async def test_command(self, ctx):
+        await ctx.send("https://eskrimlezat.rf.gd/home/about-me/")
 
     # DICE COMMAND
     @command(name="dice", aliases=["roll"])
@@ -21,7 +26,6 @@ class Fun(Cog):
         rolls = [randint(i, value) for i in range(dice)]
 
         await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum(rolls)}")
-
 
     @command(name="slap", aliases=["tenggeul"])
     async def slap_member(self, ctx, member: Member, *, reason: Optional[str] = "no reason"):
@@ -38,6 +42,7 @@ class Fun(Cog):
             self.bot.cogs_ready.ready_up("fun")
 
         await self.bot.stdout.send("Fun cogs jalan")
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
